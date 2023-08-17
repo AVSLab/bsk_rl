fb7140bf85af0ef6a65efed9cb0a33b12a36abd5
Mark Stephenson <mark2000stephenson@gmail.com>
2023-08-17 08:23:49 -0600

    Issue #33: Modular discrete actions

---
+++ bsk_rl/envs/GeneralSatelliteTasking/scenario/sat_actions.py
@@ -0,0 +1,156 @@
+from typing import Optional, Union
+
+import numpy as np
+from gymnasium import spaces
+
+from bsk_rl.envs.GeneralSatelliteTasking.scenario.environment_features import Target
+from bsk_rl.envs.GeneralSatelliteTasking.scenario.satellites import (
+    ImagingSatellite,
+    Satellite,
+)
+from bsk_rl.envs.GeneralSatelliteTasking.utils.functional import configurable
+
+
+class SatAction(Satellite):
+    """Base satellite subclass for composing actions."""
+
+    pass
+
+
+class DiscreteSatAction(SatAction):
+    def __init__(self, *args, **kwargs) -> None:
+        """Base satellite subclass for composing discrete actions. Actions are added to the satellite for each
+        DiscreteSatAction subclass, and can be accessed by index in order added."""
+        super().__init__(*args, **kwargs)
+        self.action_list = []
+        self.action_map = {}
+        self.prev_action_key = None  # Used to avoid retasking of BSK tasks
+
+    def add_action(
+        self, act_fn, act_name: Optional[str] = None, n_actions: Optional[int] = None
+    ):
+        """Add an action to the action map.
+
+        Args:
+            act_fn: Function to call when selecting action. Takes as a keyword prev_action_key, used to avoid
+                retasking of BSK models. Can accept an integer argument.
+            act_name: String to refer to action.
+            n_actions: If not none, add action n_actions times, calling it with an increasing integer argument for each
+                subsequent action.
+        """
+        if act_name is None:
+            act_name = act_fn.__name__
+        if n_actions is None:
+            self.action_map[f"{len(self.action_list)}"] = act_name
+            self.action_list.append(act_fn)
+        else:
+            self.action_map[
+                f"{len(self.action_list)}-{len(self.action_list)+n_actions-1}"
+            ] = act_name
+            for i in reversed(range(n_actions)):
+                self.action_list.append(
+                    lambda prev_action_key=None: act_fn(
+                        i, prev_action_key=prev_action_key
+                    )
+                )
+
+    def set_action(self, action: int):
+        """Function called by the environment when setting action"""
+        self._disable_timed_terminal_event()
+        self.prev_action_key = self.action_list[action](
+            prev_action_key=self.prev_action_key
+        )  # Update prev action data to avoid retasking
+
+    @property
+    def action_space(self) -> spaces.Discrete:
+        """Infer action space."""
+        return spaces.Discrete(len(self.action_list))
+
+
+def fsw_action_gen(fsw_action: str) -> DiscreteSatAction:
+    @configurable
+    class FSWAction(DiscreteSatAction):
+        def __init__(self, *args, action_duration: float = 60.0, **kwargs) -> None:
+            """Discrete action to perform a fsw action; typically this is a function decorated by @action
+
+            Args:
+                action_duration: Time to act when action selected. [s]
+            """
+            super().__init__(*args, **kwargs)
+            setattr(self, fsw_action + "_duration", action_duration)
+            self.add_action(
+                lambda prev_action_key=None: self.act(
+                    fsw_action, prev_action_key=prev_action_key
+                ),
+                act_name=fsw_action,
+            )
+
+        def act(self, fsw_action, prev_action_key=None) -> str:
+            """Activate action.
+
+            Returns:
+                action key
+            """
+            duration = getattr(self, fsw_action + "_duration")
+            self.log_info(f"{fsw_action} tasked for {duration} seconds")
+            self._disable_timed_terminal_event()
+            self._update_timed_terminal_event(
+                self.simulator.sim_time + duration, info=f"for {fsw_action}"
+            )
+            if prev_action_key != fsw_action:
+                getattr(self.fsw, fsw_action)()
+            return fsw_action
+
+    # FSWAction.__name__ = f"{FSWAction.__name__}_{fsw_action}"
+
+    return FSWAction
+
+
+# Charges the satellite
+ChargingAction = fsw_action_gen("action_charge")
+
+# Disables all actuators and control
+DriftAction = fsw_action_gen("action_drift")
+
+# Points in a specified direction while firing desat thrusters and desaturating wheels
+DesatAction = fsw_action_gen("action_desat")
+
+# Points nadir while downlinking data
+DownlinkAction = fsw_action_gen("action_downlink")
+
+
+@configurable
+class ImagingActions(DiscreteSatAction, ImagingSatellite):
+    def __init__(self, *args, n_ahead_act=10, **kwargs) -> None:
+        """Discrete action to image upcoming targets.
+
+        Args:
+            n_ahead_act: Number of actions to include in action space.
+        """
+        super().__init__(*args, **kwargs)
+        self.add_action(self.image, n_actions=n_ahead_act, act_name="image")
+
+    def image(self, target: Union[int, Target, str], prev_action_key=None) -> str:
+        """Activate imaging action.
+
+        Args:
+            target: Target, in terms of upcoming index, Target, or ID,
+
+        Returns:
+            Target ID
+        """
+        if np.issubdtype(type(target), np.integer):
+            self.log_info(f"Target index {target}")
+
+        target = self.parse_target_selection(target)
+        if target.id != prev_action_key:
+            self.task_target_for_imaging(target)
+        return target.id
+
+    def set_action(self, action: Union[int, Target, str]):
+        """Allow the satellite to be tasked by Target or target id, in addition to index"""
+        self._disable_image_event()
+        if isinstance(action, (Target, str)):
+            self.prev_action_key = self.image(action, self.prev_action_key)
+        else:
+            super().set_action(action)
--- examples/general_satellite_tasking/satellite_customization.py
+++ examples/general_satellite_tasking/satellite_customization.py
@@ -4,6 +4,7 @@ from Basilisk.architecture import bskLogging
 from Basilisk.utilities import orbitalMotion
 
 from bsk_rl.envs.GeneralSatelliteTasking.scenario import data
+from bsk_rl.envs.GeneralSatelliteTasking.scenario import sat_actions as sa
 from bsk_rl.envs.GeneralSatelliteTasking.scenario import sat_observations as so
 from bsk_rl.envs.GeneralSatelliteTasking.scenario import satellites as sats
 from bsk_rl.envs.GeneralSatelliteTasking.scenario.environment_features import (
@@ -27,8 +28,21 @@ in a satellite subclass
 
 # OPTION 1: Define a new satellite class by composing existing types.
 class CustomSatComposed(
+    # Action classes. Discrete actions are added in reverse order
+    # Thus produces an action space of the form: {'0': 'action_charge', '1': 'action_desat', '2-4': 'image'}
+    sa.ImagingActions.configure(n_ahead_act=3),
+    sa.DesatAction.configure(action_duration=120.0),
+    sa.ChargingAction.configure(action_duration=60.0),
     # Observation classes. In the vectorized observation, these will be composed in reverse order.
     # Default arguments for __init__ can be overriden with configure() to bake them into the class definition prior to instantiation.
+    # This produces an observaiton in the form:
+    #    omega_BP_P_normd:  [ 0.01489828  0.0004725  -0.08323254]
+    #    c_hat_P:  [ 0.66675533 -0.69281445  0.27467338]
+    #    r_BN_P_normd:  [ 0.09177786 -0.80203809 -0.7120501 ]
+    #    v_BN_P_normd:  [ 0.91321553 -0.11810811  0.25020653]
+    #    battery_charge_fraction:  0.740410440543005
+    #    target_obs:  {'tgt_value_0': 0.1878322060213219, 'tgt_loc_0_normd': array([ 0.21883092, -0.72328348, -0.65496109]), 'tgt_value_1': 0.8484751150377395, 'tgt_loc_1_normd': array([ 0.23371944, -0.73369242, -0.63802089]), 'tgt_value_2': 0.14482123441765427, 'tgt_loc_2_normd': array([ 0.23645694, -0.73721533, -0.63293101])}
+    #    normalized_time:  0.22505847953216376
     so.TimeState,
     so.TargetState.configure(n_ahead_observe=3),
     so.NormdPropertyState.configure(
@@ -59,76 +73,76 @@ class CustomSatComposed(
 
 
 # OPTION 2: Define a new satellite class manually, selecting a similar class as a starting point
-class CustomSatManual(sats.ImagingSatellite):
-    # Select FSW and dynamics as in option 1
-    fsw_type = fsw.SteeringImagerFSWModel
+# class CustomSatManual(sats.ImagingSatellite):
+#     # Select FSW and dynamics as in option 1
+#     fsw_type = fsw.SteeringImagerFSWModel
 
-    class CustomDynModel(dynamics.ImagingDynModel, dynamics.LOSCommDynModel):
-        pass
+#     class CustomDynModel(dynamics.ImagingDynModel, dynamics.LOSCommDynModel):
+#         pass
 
-    dyn_type = CustomDynModel
+#     dyn_type = CustomDynModel
 
-    # A more common customization requirement is designing the observation and action spaces. Three functions are most
-    # commonly overridden to achieve this: get_obs, set_action, and n_actions
+#     # A more common customization requirement is designing the observation and action spaces. Three functions are most
+#     # commonly overridden to achieve this: get_obs, set_action, and n_actions
 
-    # Define a custom observation. Various properties from the Basilisk simulation are exposed through the satellite
-    # class to make this process easier, including r_BN_B, omega_BN_B, and many more. Typically, this function should
-    # return a 1-dimensional numpy array. In this example, the satellite's dynamic state and information about upcoming
-    # targets are normalized.
-    def get_obs(self):
-        dynamic_state = np.concatenate(
-            [
-                self.dynamics.omega_BP_P / 0.03,
-                self.fsw.c_hat_P,
-                self.dynamics.r_BN_P / (orbitalMotion.REQ_EARTH * 1e3),
-                self.dynamics.v_BN_P / 7616.5,
-            ]
-        )
-        images_state = np.array(
-            [
-                np.concatenate(
-                    [
-                        [target.priority],
-                        target.location / (orbitalMotion.REQ_EARTH * 1e3),
-                    ]
-                )
-                for target in self.upcoming_targets(self.n_ahead_observe)
-            ]
-        )
-        images_state = images_state.flatten()
+#     # Define a custom observation. Various properties from the Basilisk simulation are exposed through the satellite
+#     # class to make this process easier, including r_BN_B, omega_BN_B, and many more. Typically, this function should
+#     # return a 1-dimensional numpy array. In this example, the satellite's dynamic state and information about upcoming
+#     # targets are normalized.
+#     def get_obs(self):
+#         dynamic_state = np.concatenate(
+#             [
+#                 self.dynamics.omega_BP_P / 0.03,
+#                 self.fsw.c_hat_P,
+#                 self.dynamics.r_BN_P / (orbitalMotion.REQ_EARTH * 1e3),
+#                 self.dynamics.v_BN_P / 7616.5,
+#             ]
+#         )
+#         images_state = np.array(
+#             [
+#                 np.concatenate(
+#                     [
+#                         [target.priority],
+#                         target.location / (orbitalMotion.REQ_EARTH * 1e3),
+#                     ]
+#                 )
+#                 for target in self.upcoming_targets(self.n_ahead_observe)
+#             ]
+#         )
+#         images_state = images_state.flatten()
 
-        return np.concatenate((dynamic_state, images_state))
+#         return np.concatenate((dynamic_state, images_state))
 
-    # Define a custom action function. In most discrete RL contexts, this function should accept a single integer;
-    # however, any parameterization is possible with this package. An important note: it is generally undesireable
-    # to retask the same action twice in a row as controller states will get reset. Good set_action defintions should
-    # include protections against this. In this example:
-    #   - 0: charge
-    #   - 1: desaturate
-    #   - 2+: image the (n-3)th upcoming target
-    def set_action(self, action):
-        if action == 0 and self.current_action != 0:
-            # Use functions defined in FSW with the @action decorator to interact with the Basilisk sim.
-            self.fsw.action_charge()
-            # Save data to the info dictonary for debugging help
-            self.log_info("charging tasked")
-        elif action == 1 and self.current_action != 1:
-            self.fsw.action_desat()
-            self.log_info("desat tasked")
-        else:
-            target_action = action
-            if isinstance(target_action, int):
-                target_action -= 2
-            # Use the standard ImagingSatellite tasking function
-            super().set_action(target_action)
+#     # Define a custom action function. In most discrete RL contexts, this function should accept a single integer;
+#     # however, any parameterization is possible with this package. An important note: it is generally undesireable
+#     # to retask the same action twice in a row as controller states will get reset. Good set_action defintions should
+#     # include protections against this. In this example:
+#     #   - 0: charge
+#     #   - 1: desaturate
+#     #   - 2+: image the (n-3)th upcoming target
+#     def set_action(self, action):
+#         if action == 0 and self.current_action != 0:
+#             # Use functions defined in FSW with the @action decorator to interact with the Basilisk sim.
+#             self.fsw.action_charge()
+#             # Save data to the info dictonary for debugging help
+#             self.log_info("charging tasked")
+#         if action == 1 and self.current_action != 1:
+#             self.fsw.action_desat()
+#             self.log_info("desat tasked")
+#         else:
+#             target_action = action
+#             if isinstance(target_action, int):
+#                 target_action -= 2
+#             # Use the standard ImagingSatellite tasking function
+#             super().set_action(target_action)
 
-        if action < 2:
-            self.current_action = action
+#         if action < 2:
+#             self.current_action = action
 
-    # The action space cannot be inferred; explicitly tell gymnasium how many actions the satellite can take
-    @property
-    def action_space(self):
-        return gym.spaces.Discrete(self.n_ahead_act + 2)
+#     # The action space cannot be inferred; explicitly tell gymnasium how many actions the satellite can take
+#     @property
+#     def action_space(self):
+#         return gym.spaces.Discrete(self.n_ahead_act + 2)
 
 
 # Configure the environent
@@ -144,10 +158,10 @@ sat_args = sat_type.default_sat_args(
 satellite = sat_type(
     "EO1",
     sat_args,
-    n_ahead_observe=3,
-    n_ahead_act=3,
-    variable_interval=False,
+    variable_interval=True,
 )
+# The composed satellite action space returns a human-readable action map
+print("Actions:", satellite.action_map)
 
 # Make the environment with Gymnasium
 env = gym.make(
