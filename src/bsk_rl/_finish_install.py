2d15e29018ce5682cd75107366d522c310b58c13
Mark Stephenson <mark2000stephenson@gmail.com>
2024-05-16 10:33:39 -0600

    Issue #136: Speed up root finding

--- src/bsk_rl/_finish_install.py
+++ src/bsk_rl/_finish_install.py
@@ -1,7 +1,4 @@
 import io
-import os
-import subprocess
-import sys
 import zipfile
 from pathlib import Path
 
@@ -11,28 +8,6 @@ from bsk_rl._check_bsk_version import _check_bsk_version
 
 
 def pck_install():
-    if os.uname().sysname == "Darwin" and os.uname().machine == "arm64":
-        subprocess.check_call(
-            [
-                sys.executable,
-                "-m",
-                "pip",
-                "install",
-                "--no-deps",
-                "git+https://github.com/chebpy/chebpy.git",
-            ]
-        )
-    else:
-        subprocess.check_call(
-            [
-                sys.executable,
-                "-m",
-                "pip",
-                "install",
-                "git+https://github.com/chebpy/chebpy.git",
-            ]
-        )
-
     r = requests.get(
         "https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.76.zip"
     )
