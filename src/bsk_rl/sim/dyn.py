201219a984e0366356ca24cd361b65ff36470128
Mark Stephenson <mark2000stephenson@gmail.com>
2025-03-04 15:40:02 -0700

    Support for lighting conditions

--- src/bsk_rl/sim/dyn.py
+++ src/bsk_rl/sim/dyn.py
@@ -1272,14 +1272,25 @@ class RSODynModel(BasicDynamicsModel):
     def add_rso_point(self, r_LB_B, aHat_B, theta, range):
         rso_point_model = spacecraftLocation.SpacecraftLocation()
         rso_point_model.primaryScStateInMsg.subscribeTo(self.scObject.scStateOutMsg)
-        # rso_point_model.planetInMsg.subscribeTo(
-        #     self.world.gravFactory.spiceObject.planetStateOutMsgs[self.world.body_index]
-        # )
-        rso_point_model.rEquator = 1.0  # self.simulator.world.planet.radEquator
-        rso_point_model.rPolar = 1.0  # self.simulator.world.planet.radEquator * 0.98
+
+        rso_point_model.planetInMsg.subscribeTo(
+            self.world.gravFactory.spiceObject.planetStateOutMsgs[self.world.body_index]
+        )
+        rso_point_model.rEquator = self.simulator.world.planet.radEquator
+        rso_point_model.rPolar = self.simulator.world.planet.radEquator * 0.98
+
+        rso_point_model.sunInMsg.subscribeTo(
+            self.world.gravFactory.spiceObject.planetStateOutMsgs[self.world.sun_index]
+        )
+        rso_point_model.eclipseInMsg.subscribeTo(
+            self.world.eclipseObject.eclipseOutMsgs[self.eclipse_index]
+        )
+
         rso_point_model.r_LB_B = r_LB_B
         rso_point_model.aHat_B = aHat_B
         rso_point_model.theta = theta
+        rso_point_model.theta_solar = 0.9 * np.pi / 2
+        rso_point_model.shadow_factor_limit = 0.1
         rso_point_model.maximumRange = range
         self.simulator.AddModelToTask(
             self.rso_task_name, rso_point_model, ModelPriority=1
