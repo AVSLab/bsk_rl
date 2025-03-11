bc6112a5d2ff8fb3aceb8a464b7a6961ad653a1a
Mark Stephenson <mark2000stephenson@gmail.com>
2025-03-11 16:02:34 -0600

    Illumination logging

--- src/bsk_rl/data/rso_data.py
+++ src/bsk_rl/data/rso_data.py
@@ -21,10 +21,17 @@ OBSERVER = "observer"
 
 
 class RSOInspectionData(Data):
-    def __init__(self, point_inspect_status: Optional[dict[RSOPoint, bool]] = None):
+    def __init__(
+        self,
+        point_inspect_status: Optional[dict[RSOPoint, bool]] = None,
+        point_illuminate_status: Optional[dict[RSOPoint, bool]] = None,
+    ):
         if point_inspect_status is None:
             point_inspect_status = {}
         self.point_inspect_status = point_inspect_status
+        if point_illuminate_status is None:
+            point_illuminate_status = {}
+        self.point_illuminate_status = point_illuminate_status
 
     def __add__(self, other: "RSOInspectionData"):
         point_inspect_status = {}
@@ -35,7 +42,27 @@ class RSOInspectionData(Data):
             else:
                 point_inspect_status[point] = point_inspect_status[point] or access
 
-        return RSOInspectionData(point_inspect_status)
+        point_illuminate_status = {}
+        point_illuminate_status.update(self.point_illuminate_status)
+        for point, access in other.point_illuminate_status.items():
+            if point not in point_illuminate_status:
+                point_illuminate_status[point] = access
+            else:
+                point_illuminate_status[point] = (
+                    point_illuminate_status[point] or access
+                )
+
+        return RSOInspectionData(point_inspect_status, point_illuminate_status)
+
+    @property
+    def num_points_inspected(self):
+        """Number of points inspected."""
+        return sum(self.point_inspect_status.values())
+
+    @property
+    def num_points_illuminated(self):
+        """Number of points illuminated."""
+        return sum(self.point_illuminate_status.values())
 
 
 class RSOInspectionDataStore(DataStore):
@@ -44,6 +71,7 @@ class RSOInspectionDataStore(DataStore):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.point_access_recorders = []
+        self.illumination_recorders = []
         self.storage_recorder = None
 
         if issubclass(self.satellite.dyn_type, RSOImagingDynModel):
@@ -57,16 +85,24 @@ class RSOInspectionDataStore(DataStore):
             self.satellite.dynamics.task_name, recorder, ModelPriority=1000
         )
 
-    def add_point_access_recorder(self, recorder):
+    def add_point_access_recorder(self, recorder, illumination_recorder):
         self.point_access_recorders.append(recorder)
+        self.illumination_recorders.append(illumination_recorder)
         self.satellite.simulator.AddModelToTask(
             self.satellite.dynamics.task_name, recorder, ModelPriority=1000
+        )
+        self.satellite.simulator.AddModelToTask(
+            self.satellite.dynamics.task_name,
+            illumination_recorder,
+            ModelPriority=1000,
         )
 
     def clear_recorders(self):
         if self.storage_recorder:
             self.storage_recorder.clear()
         for recorder in self.point_access_recorders:
+            recorder.clear()
+        for recorder in self.illumination_recorders:
             recorder.clear()
 
     def get_log_state(self) -> list[list[bool]]:
@@ -90,13 +126,19 @@ class RSOInspectionDataStore(DataStore):
             inspected = np.logical_and(imaging_req, recorder.hasAccess)
             inspected_logs.append(list(np.array(inspected)))
 
+        illuminated_logs = []
+        for recorder in self.illumination_recorders:
+            illuminated_logs.append(list(np.array(recorder.hasAccess)))
+
         self.clear_recorders()
 
-        return inspected_logs
+        return inspected_logs, illuminated_logs
 
-    def compare_log_states(self, _, inspected_logs) -> Data:
+    def compare_log_states(self, _, logs) -> Data:
         if self.role == RSO:
             return RSOInspectionData()
+
+        inspected_logs, illuminated_logs = logs
 
         point_inspect_status = {}
         for rso_point, log in zip(
@@ -104,6 +146,13 @@ class RSOInspectionDataStore(DataStore):
         ):
             if any(log):
                 point_inspect_status[rso_point] = True
+
+        point_illuminate_status = {}
+        for rso_point, log in zip(
+            self.data.point_illuminate_status.keys(), illuminated_logs
+        ):
+            if any(log):
+                point_illuminate_status[rso_point] = True
 
         self.update_point_colors(
             [
@@ -118,7 +167,7 @@ class RSOInspectionDataStore(DataStore):
                 f"Inspected {len(point_inspect_status)} points this step"
             )
 
-        return RSOInspectionData(point_inspect_status)
+        return RSOInspectionData(point_inspect_status, point_illuminate_status)
 
     @vizard.visualize
     def update_point_colors(self, rso_points, vizInstance=None, vizSupport=None):
@@ -132,11 +181,17 @@ class RSOInspectionReward(GlobalReward):
     datastore_type = RSOInspectionDataStore
 
     def __init__(
-        self, inspection_reward_scale: float = 1.0, completion_bonus: float = 0.0
+        self,
+        inspection_reward_scale: float = 1.0,
+        completion_bonus: float = 0.0,
+        completion_threshold=0.95,
+        min_illuminated_for_completion=0.4,
     ):
         super().__init__()
         self.completion_bonus = completion_bonus
         self.inspection_reward_scale = inspection_reward_scale
+        self.completion_threshold = completion_threshold
+        self.min_illuminated_for_completion = min_illuminated_for_completion
 
     def reset_overwrite_previous(self) -> None:
         super().reset_overwrite_previous()
@@ -154,19 +209,23 @@ class RSOInspectionReward(GlobalReward):
             )
             for rso_point_model in self.scenario.rso.dynamics.rso_points:
                 observer.data_store.add_point_access_recorder(
-                    rso_point_model.accessOutMsgs[i].recorder()
+                    rso_point_model.accessOutMsgs[i].recorder(),
+                    rso_point_model.illuminationOutMsgs[i].recorder(),
                 )
 
     def initial_data(self, satellite: Satellite) -> Data:
         if not issubclass(satellite.dyn_type, RSOImagingDynModel):
             return RSOInspectionData()
 
-        return RSOInspectionData({point: False for point in self.scenario.rso_points})
+        return RSOInspectionData(
+            {point: False for point in self.scenario.rso_points},
+            {point: False for point in self.scenario.rso_points},
+        )
 
     def calculate_reward(self, new_data_dict: dict[str, Data]) -> dict[str, float]:
         total_points = len(self.scenario.rso_points)
         reward = {}
-        total_new_points = 0
+        total_data = self.data_type() + self.data
         for satellite_id, data in new_data_dict.items():
             if len(data.point_inspect_status) == 0:
                 continue
@@ -182,13 +241,26 @@ class RSOInspectionReward(GlobalReward):
             reward[satellite_id] = (
                 new_points / total_points * self.inspection_reward_scale
             )
-            total_new_points += new_points
+            total_data += data
+
+        # Check for completion bonus
+        min_illuminated_met = total_data.num_points_illuminated > (
+            len(self.scenario.rso_points) * self.min_illuminated_for_completion
+        )
+
+        imaged_fraction_met = (
+            total_data.num_points_inspected
+            > self.completion_threshold * total_data.num_points_illuminated
+        )
+
         if (
-            sum(self.data.point_inspect_status.values()) + total_new_points
-            == len(self.scenario.rso_points)
+            min_illuminated_met
+            and imaged_fraction_met
             and not self.bonus_reward_yielded
         ):
-            logger.info("All points inspected! Awarding completion bonus.")
+            logger.info(
+                f"{total_data.num_points_inspected} points inspected / {total_data.num_points_illuminated} illuminated! Awarding completion bonus."
+            )
             for satellite_id in self.cum_reward:
                 reward[satellite_id] = (
                     reward.get(satellite_id, 0.0) + self.completion_bonus
