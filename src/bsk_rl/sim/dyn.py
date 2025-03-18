92b486d7e115e38bbaa27dd11a7ce6937313ea62
Daniel Huterer Prats <dahu1128@colorado.edu>
2025-03-17 18:50:05 -0600

    add setup of different modules RW, SolarPanels, StorageUnit, etc to BasicDynModel and BasicTargetDynModel in dyn.py to treat the rso-targets as agents in the simulation.

--- src/bsk_rl/sim/dyn.py
+++ src/bsk_rl/sim/dyn.py
@@ -711,6 +711,418 @@ class BasicDynamicsModel(DynamicsModel):
             )
             self.powerMonitor.addPowerNodeToModel(powerRW.nodePowerOutMsg)
 
+class BasicTargetDynamicsModel(BasicDynamicsModel):
+    @default_args(
+        mass=330,
+        width=1.38,
+        depth=1.04,
+        height=1.58,
+        sigma_init=lambda: random_tumble(maxSpinRate=0.0001)[0],
+        omega_init=lambda: random_tumble(maxSpinRate=0.0001)[1],
+        rN=None,
+        vN=None,
+        oe=random_orbit,
+        mu=orbitalMotion.MU_EARTH * 1e9,
+    )
+    def setup_spacecraft_hub(
+        self,
+        mass: float,
+        width: float,
+        depth: float,
+        height: float,
+        sigma_init: Iterable[float],
+        omega_init: Iterable[float],
+        oe: Optional[Iterable[float]],
+        rN: Optional[Iterable[float]],
+        vN: Optional[Iterable[float]],
+        mu: float,
+        priority: int = 2000,
+        **kwargs,
+    ) -> None:
+        """Set up the spacecraft hub physical properties and state.
+
+        The hub is assumed to be a uniform-density rectangular prism with the center of
+        mass at the center.
+
+        Args:
+            mass: [kg] Hub mass.
+            width: [m] Hub width.
+            depth: [m] Hub depth.
+            height: [m] Hub height.
+            sigma_init: Initial attitude MRP.
+            omega_init: [rad/s] Initial body rate.
+            oe: Orbital element tuple of (semimajor axis [km], eccentricity, inclination
+                [rad], ascending node [rad], argument of periapsis [rad], initial true
+                anomaly [rad]). Either ``oe`` and ``mu`` or ``rN`` and ``vN`` must be
+                provided, but not both.
+            mu: Gravitational parameter (used only with ``oe``).
+            rN: [m] Initial inertial position.
+            vN: [m/s] Initial inertial velocity.
+            priority: Model priority.
+            kwargs: Passed to other setup functions.
+        """
+        if rN is not None and vN is not None and oe is None:
+            pass
+        elif oe is not None and rN is None and vN is None:
+            rN, vN = orbitalMotion.elem2rv(mu, oe)
+        else:
+            raise (KeyError("Orbit is overspecified. Provide either (rN, vN) or oe"))
+
+        self.scObject = spacecraft.Spacecraft()
+        self.scObject.ModelTag = self.satellite.name
+
+        Ixx = 1.0 / 12.0 * mass * (width**2.0 + depth**2.0)
+        Iyy = 1.0 / 12.0 * mass * (depth**2.0 + height**2.0)
+        Izz = 1.0 / 12.0 * mass * (width**2.0 + height**2.0)
+        self.I_mat = [Ixx, 0.0, 0.0, 0.0, Iyy, 0.0, 0.0, 0.0, Izz]
+
+        self.scObject.hub.mHub = mass  # kg
+        self.scObject.hub.IHubPntBc_B = unitTestSupport.np2EigenMatrix3d(self.I_mat)
+
+        # Set the initial attitude and position
+        self.scObject.hub.sigma_BNInit = sigma_init
+        self.scObject.hub.omega_BN_BInit = omega_init
+        self.scObject.hub.r_CN_NInit = unitTestSupport.np2EigenVectorXd(rN)
+        self.scObject.hub.v_CN_NInit = unitTestSupport.np2EigenVectorXd(vN)
+
+        self.simulator.AddModelToTask(
+            self.task_name, self.scObject, ModelPriority=priority
+        )
+
+        self.setup_gravity_bodies()
+        self.setup_disturbance_torque(**kwargs)
+        self.setup_density_model()
+
+    def setup_gravity_bodies(self) -> None:
+        """Set up gravitational bodies from the :class:`~bsk_rl.sim.world.WorldModel` to included in the simulation."""
+        self.scObject.gravField.gravBodies = spacecraft.GravBodyVector(
+            list(self.world.gravFactory.gravBodies.values())
+        )
+
+    @default_args(disturbance_vector=None)
+    def setup_disturbance_torque(
+        self, disturbance_vector: Optional[Iterable[float]] = None, **kwargs
+    ) -> None:
+        """Set up a constant disturbance torque acting on the satellite.
+
+        Args:
+            disturbance_vector: [N*m] Constant disturbance torque in the body frame.
+            kwargs: Passed to other setup functions.
+        """
+        if disturbance_vector is None:
+            disturbance_vector = np.array([0, 0, 0])
+        self.extForceTorqueObject = extForceTorque.ExtForceTorque()
+        self.extForceTorqueObject.ModelTag = "DisturbanceTorque"
+        self.extForceTorqueObject.extTorquePntB_B = disturbance_vector
+        self.scObject.addDynamicEffector(self.extForceTorqueObject)
+
+    def setup_density_model(self) -> None:
+        """Set up the atmospheric density model effector."""
+        self.world.densityModel.addSpacecraftToModel(self.scObject.scStateOutMsg)
+
+    @default_args(dragCoeff=2.2)
+    def setup_drag_effector(
+        self,
+        width: float,
+        depth: float,
+        height: float,
+        panelArea: float,
+        dragCoeff: float,
+        priority: int = 999,
+        **kwargs,
+    ) -> None:
+        """Set up the satellite drag effector.
+
+        The drag effector causes aerodynamic forces and torques to act on the satellite.
+        For purposes of this model, the satellite is assumed to be a rectangular prism
+        with a solar panel on one end.
+
+        Args:
+            width: [m] Hub width.
+            depth: [m] Hub depth.
+            height: [m] Hub height.
+            panelArea: [m^2] Solar panel surface area.
+            dragCoeff: Drag coefficient.
+            priority: Model priority.
+            kwargs: Passed to other setup functions.
+        """
+        self.dragEffector = facetDragDynamicEffector.FacetDragDynamicEffector()
+        self.dragEffector.ModelTag = "FacetDrag"
+        #  Set up the geometry of a small satellite, starting w/ bus
+        self.dragEffector.addFacet(
+            width * depth, dragCoeff, [1, 0, 0], [height / 2, 0.0, 0]
+        )
+        self.dragEffector.addFacet(
+            width * depth, dragCoeff, [-1, 0, 0], [height / 2, 0.0, 0]
+        )
+        self.dragEffector.addFacet(
+            height * width, dragCoeff, [0, 1, 0], [0, depth / 2, 0]
+        )
+        self.dragEffector.addFacet(
+            height * width, dragCoeff, [0, -1, 0], [0, -depth / 2, 0]
+        )
+        self.dragEffector.addFacet(
+            height * depth, dragCoeff, [0, 0, 1], [0, 0, width / 2]
+        )
+        self.dragEffector.addFacet(
+            height * depth, dragCoeff, [0, 0, -1], [0, 0, -width / 2]
+        )
+        # Add solar panels
+        self.dragEffector.addFacet(
+            panelArea / 2,
+            dragCoeff,
+            [0, 1, 0],
+            [0, height, 0],
+        )
+        self.dragEffector.addFacet(
+            panelArea / 2,
+            dragCoeff,
+            [0, -1, 0],
+            [0, height, 0],
+        )
+        self.dragEffector.atmoDensInMsg.subscribeTo(
+            self.world.densityModel.envOutMsgs[-1]
+        )
+        self.scObject.addDynamicEffector(self.dragEffector)
+
+        self.simulator.AddModelToTask(
+            self.task_name, self.dragEffector, ModelPriority=priority
+        )
+
+    @default_args(
+        dataStorageCapacity=20 * 8e6,
+        bufferNames=None,
+        storageUnitValidCheck=False,
+        storageInit=0,
+    )
+    def setup_storage_unit(
+        self,
+        dataStorageCapacity: int,
+        storageUnitValidCheck: bool,
+        storageInit: int,
+        transmitterNumBuffers: Optional[int] = None,
+        bufferNames: Optional[Iterable[str]] = None,
+        priority: int = 699,
+        **kwargs,
+    ) -> None:
+        """Configure the storage unit and its buffers.
+
+        Separate buffers can be used to track imaging of different targets. Often, the
+        buffer names will be set up by satellite based on the scenario configuration.
+
+        Args:
+            dataStorageCapacity: [bits] Maximum data that can be stored.
+            transmitterNumBuffers: Number of unit buffers. Not necessary if ``bufferNames``
+                are given.
+            bufferNames: List of buffer names to use. Named by number if ``None``.
+            storageUnitValidCheck: If ``True``, enforce that the storage level is below
+                the storage capacity when checking aliveness.
+            storageInit: [bits] Initial storage level.
+            priority: Model priority.
+            kwargs: Passed to other setup functions.
+        """
+        self.storageUnit = partitionedStorageUnit.PartitionedStorageUnit()
+        self.storageUnit.ModelTag = "storageUnit" + self.satellite.name
+        self.storageUnit.storageCapacity = dataStorageCapacity  # bits
+        self.storageUnit.addDataNodeToModel(self.instrument.nodeDataOutMsg)
+        self.storageUnit.addDataNodeToModel(self.transmitter.nodeDataOutMsg)
+        self.storageUnitValidCheck = storageUnitValidCheck
+        # Add all of the targets to the data buffer
+        if bufferNames is None:
+            for buffer_idx in range(transmitterNumBuffers):
+                self.storageUnit.addPartition(str(buffer_idx))
+        else:
+            if transmitterNumBuffers is not None and transmitterNumBuffers != len(
+                bufferNames
+            ):
+                raise ValueError(
+                    "transmitterNumBuffers cannot be different than len(bufferNames)."
+                )
+            for buffer_name in bufferNames:
+                self.storageUnit.addPartition(buffer_name)
+
+        if storageInit != 0:
+            if storageInit > dataStorageCapacity or storageInit < 0:
+                self.logger.warning(
+                    f"Initial storage level {storageInit} incompatible with its capacity {dataStorageCapacity}."
+                )
+            self.storageUnit.setDataBuffer(["STORED DATA"], [int(storageInit)])
+
+        # Add the storage unit to the transmitter
+        self.transmitter.addStorageUnitToTransmitter(
+            self.storageUnit.storageUnitDataOutMsg
+        )
+
+        self.simulator.AddModelToTask(
+            self.task_name, self.storageUnit, ModelPriority=priority
+        )
+
+    def _setup_dynamics_objects(self, **kwargs) -> None:
+        super()._setup_dynamics_objects(**kwargs)
+        self.setup_instrument(**kwargs)
+        self.setup_transmitter(**kwargs)
+        self.setup_instrument_power_sink(**kwargs)
+        self.setup_transmitter_power_sink(**kwargs)
+        self.setup_storage_unit(**kwargs)
+
+    def setup_simple_nav_object(self, priority: int = 699):
+        # Set up simple navigation for target SC
+        self.simpleNavObject = simpleNav.SimpleNav()
+        self.simpleNavObject.ModelTag = "SimpleTargetNav"
+        self.simpleNavObject.scStateInMsg.subscribeTo(self.scObject.scStateOutMsg)
+        self.simulator.AddModelToTask(
+            self.task_name, self.simpleNavObject, ModelPriority=priority
+        )
+
+    def setup_eclipse_object(self) -> None:
+        """Add the spacecraft to the eclipse module."""
+        self.world.eclipseObject.addSpacecraftToModel(self.scObject.scStateOutMsg)
+        self.eclipse_index = len(self.world.eclipseObject.eclipseOutMsgs) - 1
+
+    @default_args(
+        panelArea=2 * 1.0 * 0.5,
+        panelEfficiency=0.20,
+        nHat_B=np.array([0, 0, -1]),
+    )
+    def setup_solar_panel(
+        self,
+        panelArea: float,
+        panelEfficiency: float,
+        nHat_B: Iterable[float],
+        priority: int = 898,
+        **kwargs,
+    ) -> None:
+        """Set the solar panel parameters for power generation.
+
+        Power generation takes into account panel size and efficiency, the eclipse
+        state, and the angle of solar incidence.
+
+        Args:
+            panelArea: [m^2] Solar panel area.
+            panelEfficiency: Efficiency coefficient of solar to electrical power
+                conversion.
+            nHat_B: Body-fixed array normal vector.
+            priority: Model priority.
+            kwargs: Passed to other setup functions.
+        """
+        self.solarPanel = simpleSolarPanel.SimpleSolarPanel()
+        self.solarPanel.ModelTag = "solarPanel" + self.satellite.name
+        self.solarPanel.stateInMsg.subscribeTo(self.scObject.scStateOutMsg)
+        self.solarPanel.sunEclipseInMsg.subscribeTo(
+            self.world.eclipseObject.eclipseOutMsgs[self.eclipse_index]
+        )
+        self.solarPanel.sunInMsg.subscribeTo(
+            self.world.gravFactory.spiceObject.planetStateOutMsgs[self.world.sun_index]
+        )
+        self.solarPanel.setPanelParameters(
+            unitTestSupport.np2EigenVectorXd(nHat_B),
+            panelArea,
+            panelEfficiency,
+        )
+        self.simulator.AddModelToTask(
+            self.task_name, self.solarPanel, ModelPriority=priority
+        )
+
+    @default_args(
+        batteryStorageCapacity=80.0 * 3600.0,
+        storedCharge_Init=lambda: np.random.uniform(30.0 * 3600.0, 70.0 * 3600.0),
+    )
+    def setup_battery(
+        self,
+        batteryStorageCapacity: float,
+        storedCharge_Init: float,
+        priority: int = 799,
+        **kwargs,
+    ) -> None:
+        """Set the battery model parameters.
+
+        Args:
+            batteryStorageCapacity: [W*s] Maximum battery charge.
+            storedCharge_Init: [W*s] Initial battery charge.
+            priority: Model priority.
+            kwargs: Passed to other setup functions.
+        """
+        self.powerMonitor = simpleBattery.SimpleBattery()
+        self.powerMonitor.ModelTag = "powerMonitor"
+        self.powerMonitor.storageCapacity = batteryStorageCapacity
+        if storedCharge_Init > batteryStorageCapacity or storedCharge_Init < 0:
+            self.logger.warning(
+                f"Battery initial charge {storedCharge_Init} incompatible with its capacity {batteryStorageCapacity}."
+            )
+        self.powerMonitor.storedCharge_Init = storedCharge_Init
+        self.powerMonitor.addPowerNodeToModel(self.solarPanel.nodePowerOutMsg)
+        self.simulator.AddModelToTask(
+            self.task_name, self.powerMonitor, ModelPriority=priority
+        )
+
+    @aliveness_checker
+    def battery_valid(self) -> bool:
+        """Check if the battery has charge remaining.
+
+        Note that this check is instantaneous. If a satellite runs out of power during a
+        environment step but then recharges to have positive power at the end of the step,
+        the satellite will still be considered alive.
+        """
+        return self.battery_charge > 0
+
+    @default_args(basePowerDraw=0.0)
+    def setup_power_sink(
+        self, basePowerDraw: float, priority: int = 897, **kwargs
+    ) -> None:
+        """Set the instrument power sink parameters.
+
+        Args:
+            basePowerDraw: [W] Baseline satellite power draw. Should be negative.
+            priority: Model priority.
+            kwargs: Passed to other setup functions.
+        """
+        if basePowerDraw > 0:
+            self.logger.warning("basePowerDraw should probably be zero or negative.")
+        self.basePowerSink = simplePowerSink.SimplePowerSink()
+        self.basePowerSink.ModelTag = "basePowerSink" + self.satellite.name
+        self.basePowerSink.nodePowerOut = basePowerDraw  # Watts
+        self.simulator.AddModelToTask(
+            self.task_name, self.basePowerSink, ModelPriority=priority
+        )
+        self.powerMonitor.addPowerNodeToModel(self.basePowerSink.nodePowerOutMsg)
+        self.basePowerSink.powerStatus = 1
+
+    @default_args(
+        rwBasePower=0.4, rwMechToElecEfficiency=0.0, rwElecToMechEfficiency=0.5
+    )
+    def setup_reaction_wheel_power(
+        self,
+        rwBasePower: float,
+        rwMechToElecEfficiency: float,
+        rwElecToMechEfficiency: float,
+        priority: int = 987,
+        **kwargs,
+    ) -> None:
+        """Set the reaction wheel power draw.
+
+        Args:
+            rwBasePower: [W] Constant power draw when operational.
+            rwMechToElecEfficiency: Efficiency factor to convert mechanical power to
+                electrical power.
+            rwElecToMechEfficiency: Efficiency factor to convert electrical power to
+                mechanical power.
+            priority: Model priority.
+            kwargs: Passed to other setup functions.
+        """
+        self.rwPowerList = []
+        for i_device in range(self.rwFactory.getNumOfDevices()):
+            powerRW = ReactionWheelPower.ReactionWheelPower()
+            powerRW.ModelTag = "rwPower" + str(i_device)
+            powerRW.basePowerNeed = rwBasePower
+            powerRW.rwStateInMsg.subscribeTo(self.rwStateEffector.rwOutMsgs[i_device])
+            powerRW.mechToElecEfficiency = rwMechToElecEfficiency
+            powerRW.elecToMechEfficiency = rwElecToMechEfficiency
+            self.rwPowerList.append(powerRW)
+            self.simulator.AddModelToTask(
+                self.task_name, powerRW, ModelPriority=(priority - i_device)
+            )
+            self.powerMonitor.addPowerNodeToModel(powerRW.nodePowerOutMsg)
+
 
 class LOSCommDynModel(BasicDynamicsModel):
     """For evaluating line-of-sight connections between satellites for communication."""
