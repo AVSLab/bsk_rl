165305ce1e1990168a6911fb41e449f661e510a5
Daniel Huterer Prats <dahu1128@colorado.edu>
2025-03-17 18:44:10 -0600

    Add the random orbit generator and create the scanning satellite and the targets in Polaris_environment.py

--- examples/Polaris_environment.py
+++ examples/Polaris_environment.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 from requests.packages import target
+import time
 
 import os
 import numpy as np
@@ -8,11 +9,19 @@ import numpy as np
 data_dir = "data"
 os.makedirs(data_dir, exist_ok=True)
 
-from bsk_rl import act, data, obs, scene, sats
+from bsk_rl import act, data, obs, scene, sats, utils
 from bsk_rl.sim import dyn, fsw
+
+from Basilisk.utilities import (
+    macros,
+    orbitalMotion,
+)
 
 from Basilisk.architecture import bskLogging
 bskLogging.setDefaultLogLevel(bskLogging.BSK_WARNING)
+
+n_targets = 1
+total_time = 5700 # 5700.0  # approximately 1 orbit
 
 class MyScanningSatellite(sats.Satellite):
     observation_spec = [
@@ -53,8 +62,72 @@ class MyTargetSatellite(sats.Satellite):
     ]
     dyn_type = dyn.BasicTargetDynamicsModel  # Passed as a type
     fsw_type = fsw.BasicTargetFSWModel
+def custom_oe_randomizer():
+    rLEO = 7000. * 1000    # Minimum semi-major axis (LEO) in meters
+    rGEO = 42164. * 1000   # Maximum semi-major axis (GEO) in meters
+
+
+    oe = orbitalMotion.ClassicElements()
+    oe.a = np.random.uniform(rLEO, rGEO)  # Random semi-major axis between LEO and GEO
+    if oe.a < 1.5*rLEO:
+        oe.e = np.random.uniform(0.0, 0.1)    # Random eccentricity (allowing less elliptical orbits when near LEO)
+    else:
+        oe.e = np.random.uniform(0.0, 0.2)    # Random eccentricity (allowing slightly elliptical orbits)
+    oe.i = np.random.uniform(0, 180) * macros.D2R  # Random inclination up to 180 degrees
+    oe.Omega = np.random.uniform(0, 360) * macros.D2R  # Random RAAN
+    oe.omega = np.random.uniform(0, 360) * macros.D2R  # Random argument of perigee
+    oe.f = np.random.uniform(0, 360) * macros.D2R  # Random true anomaly
+    # print('randomized orbital elements: oe.a, oe.i, oe.e', oe.a, oe.i, oe.e, 'oe.Omega, oe.omega, oe.f', oe.Omega, oe.omega, oe.f)
+    return oe
+
+target_args=dict(oe=custom_oe_randomizer, batteryStorageCapacity = 80.0 * 3600.0*1000, storedCharge_Init = 80.0 * 3600.0*900 )
 # Make the satellite
-sat = MyScanningSatellite(name="SO1", sat_args=sat_args) # SO1 for satellite observer 1
+sat = MyScanningSatellite(name="SS1", sat_args=sat_args) # SO1 for satellite observer 1
+target0 = dict(
+    oe=custom_oe_randomizer,
+    batteryStorageCapacity=80.0 * 3600.0 * 1000,
+    storedCharge_Init=80.0 * 3600.0 * 900
+)
+#
+# target1 = dict(
+#     oe=custom_oe_randomizer,
+#     batteryStorageCapacity=80.0 * 3600.0 * 1000,
+#     storedCharge_Init=80.0 * 3600.0 * 900
+# )
+# target0 = dict(
+#     oe=custom_oe_randomizer(random_seed = 9),
+#     batteryStorageCapacity=80.0 * 3600.0 * 1000,
+#     storedCharge_Init=80.0 * 3600.0 * 900
+# )
+#
+# target1 = dict(
+#     oe=custom_oe_randomizer(random_seed = 7),
+#     batteryStorageCapacity=80.0 * 3600.0 * 1000,
+#     storedCharge_Init=80.0 * 3600.0 * 900
+# )
+# target2 = dict(
+#     oe=custom_oe_randomizer(random_seed = 15),
+#     batteryStorageCapacity=80.0 * 3600.0 * 1000,
+#     storedCharge_Init=80.0 * 3600.0 * 900
+# )
+# target3 = dict(
+#     oe=custom_oe_randomizer(random_seed = 18),
+#     batteryStorageCapacity=80.0 * 3600.0 * 1000,
+#     storedCharge_Init=80.0 * 3600.0 * 900
+# )
+# target4 = dict(
+#     oe=custom_oe_randomizer(random_seed = 12),
+#     batteryStorageCapacity=80.0 * 3600.0 * 1000,
+#     storedCharge_Init=80.0 * 3600.0 * 900
+# )
+
+# targets = [MyTargetSatellite(name=f"target_0", sat_args=target0), MyTargetSatellite(name=f"target_1", sat_args=target1), MyTargetSatellite(name=f"target_2", sat_args=target2), MyTargetSatellite(name=f"target_3", sat_args=target3), MyTargetSatellite(name=f"target_4", sat_args=target4)]
+# targets = [MyTargetSatellite(name=f"target_0", sat_args=target0), MyTargetSatellite(name=f"target_1", sat_args=target1)]
+targets = [MyTargetSatellite(name=f"target_0", sat_args=target0)]
+
+# targets = [MyTargetSatellite(name=f"target_{i}", sat_args=target_args) for i in range(n_targets)] # TODO: this creates the same IC of oe for all targets
+
+all_sat = [sat] + targets   #oe = lambda: random_orbit(alt=np.random.uniform(1000,2000)))
 
 env = gym.make(
     "SatelliteTasking-v1",
